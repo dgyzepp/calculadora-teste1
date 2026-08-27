a59751ae10e56e6940a39b1f4293ca0da46d51ba
David <davidborborema2019@gmail.com>
2026-08-27 16:04:39 -0300

    teste

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿namespace calculadora
+{
+    public class Program 
+    {
+       public static void Main()
+        {
+            Console.WriteLine("-------CALCULADORA INICIAL---------");
+
+           Console.WriteLine("Digite o primeiro número: ");
+           int num1 = Convert.ToInt32(Console.ReadLine());
+
+           Console.WriteLine("Digite o segundo número: ");
+           int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            int soma = num1 + num2;
+            int sub = num1 - num2;
+            int mult = num1 * num2;
+            int div = num1 / num2;
+            int mod = num1 % num2;
+
+            Console.WriteLine("O valor da soma é: " + soma);
+            Console.WriteLine("O valor da subtração é: " + sub);
+            Console.WriteLine("O valor da multiplicação é: " + mult);
+            Console.WriteLine("O valor da divisão é: " + div);
+            Console.WriteLine("O valor do módulo é: " + mod);
+
+
+
+        }
+        
+
+
+    }
+}
